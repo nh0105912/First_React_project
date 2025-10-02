c5b54d81b39e1c1560c47c9b83495ba73a080866
Noorul Hadi <nh0105912@gmail.com>
2025-10-02 02:41:41 -0700

    add some file to json and vite file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// change 'your-repo-name' to your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
+  base: '/Sports_React_project/',
 })
